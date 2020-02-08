22c5ea7a419620c368611dd2cc5b1b7d7ee23cb7
Thomas Cannon <tcannon00@gmail.com>
2020-02-08 16:21:52 -0500

    fix values parsing

--- lib/ruby_red_ink/values.rb
+++ lib/ruby_red_ink/values.rb
@@ -2,27 +2,26 @@ module RubyRedInk
   module Values
     VOID = :VOID_VALUE
 
-    def parse(value)
+    def self.parse(value)
       return parse_divergent(value) if is_divergent?(value)
-      return VOID if is_void?
+      return VOID if is_void?(value)
       return parse_string(value) if is_string?(value)
       return value if value.is_a?(Numeric)
     end
 
-    def is_string?(value)
-      (value.start_with?("^") || value == "\n")
+    def self.is_string?(value)
+      value.is_a?(String) && (value.start_with?("^") || value == "\n")
     end
 
-    def is_divergent?(value)
-      value.is_a?(Hash)
-      value.has_key?("^->")
+    def self.is_divergent?(value)
+      value.is_a?(Hash) && value.has_key?("^->")
     end
 
-    def parse_divergent(value)
+    def self.parse_divergent(value)
       value["^->"]
     end
 
-    def parse_string(value)
+    def self.parse_string(value)
       if value.start_with?("^")
         value[1..-1]
       else
@@ -30,7 +29,7 @@ module RubyRedInk
       end
     end
 
-    def is_void?(value)
+    def self.is_void?(value)
       value == "void"
     end
   end
