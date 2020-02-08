b18822dd677ea0b6c6e84d51b9d70e3b8b9bd4a2
Thomas Cannon <tcannon00@gmail.com>
2020-02-08 00:27:30 -0500

    Test helper setup, add test helper for parsing JSON

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,11 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "ruby_red_ink"
 
+Bundler.require(:default, :development)
+
 require "minitest/autorun"
+require 'json'
+
+def load_json_export(filepath = "test/fixtures/the-intercept.js.json")
+  JSON.parse(File.read(filepath))
+end
