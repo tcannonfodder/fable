86fe3f59a918b529c1d6072c6cd31070f4af6440
Thomas Cannon <tcannon00@gmail.com>
2020-02-08 16:22:17 -0500

    Add test helper method to build a container

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,3 +9,7 @@ require 'json'
 def load_json_export(filepath = "test/fixtures/the-intercept.js.json")
   JSON.parse(File.read(filepath))
 end
+
+def build_container(object)
+  RubyRedInk::Container.new(object)
+end
