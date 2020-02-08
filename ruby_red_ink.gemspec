9aa4ad25bc8ce217ca32eff7bb357443cb6c4815
Thomas Cannon <tcannon00@gmail.com>
2020-02-08 17:25:31 -0500

    Add `highline` as a development dependency to for "zork mode" program
    * "Zork Mode" will be a basic command-line app that uses the library
      to run a story

--- ruby_red_ink.gemspec
+++ ruby_red_ink.gemspec
@@ -28,4 +28,5 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_development_dependency "byebug"
+  spec.add_development_dependency "highline"
 end
---
+++ zork_mode.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default, :development)
+
+require 'ruby_red_ink'
+
+require 'highline'
+
+# Basic usage
+
+cli = HighLine.new
+
+## Using colored indices on Menus
+
+HighLine::Menu.index_color   = :rgb_77bbff # set default index color
+
+cli.choose do |menu|
+  # menu.index_color  = :rgb_999999      # override default color of index
+                                       # you can also use constants like :blue
+  menu.prompt = "Please choose your favorite programming language?  "
+  menu.choice(:ruby) { cli.say("Good choice!") }
+  menu.choices(:python, :perl) { cli.say("Not from around here, are you?") }
+end
