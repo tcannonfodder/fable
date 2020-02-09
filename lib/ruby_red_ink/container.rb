277c21a538cc50a579ad6a81f6adc6a9530a9dbf
Thomas Cannon <tcannon00@gmail.com>
2020-02-08 22:51:32 -0500

    Container values are now elements, containers build their path string
    * Containers contain a mixture of elements, so we need to generalize
      the name
    * Having containers build their path string as they're built helps
      us ensure we have state tracking and easier tracibility

--- lib/ruby_red_ink/container.rb
+++ lib/ruby_red_ink/container.rb
@@ -1,11 +1,13 @@
 module RubyRedInk
   class Container
-    attr_accessor :original_object, :values, :nested_containers, :final_attribute, :record_visits, :record_turn_index, :count_start_only
+    attr_accessor :original_object, :elements, :nested_containers, :final_attribute, :record_visits, :record_turn_index, :count_start_only, :path_string
 
-    def initialize(original_object)
+    def initialize(original_object, parent_path_string, fallback_identifier = 0)
       self.original_object = original_object
       self.final_attribute = original_object.last
-      process_values
+
+      self.path_string = Path.append_path_string(parent_path_string, (name || fallback_identifier))
+      process_elements
       process_nested_containers
       process_bit_flags
     end
@@ -28,9 +30,21 @@ module RubyRedInk
       final_attribute["#f"]
     end
 
-    def process_values
-      self.values = original_object[0..-2].map do |value|
-        Values.parse(value)
+    def process_elements
+      self.elements = []
+
+      original_object[0..-2].each_with_index do |value, index|
+        if value.is_a?(Array)
+          elements << self.class.new(value, path_string, index)
+          next
+        end
+
+        if ControlCommands.is_control_command?(value)
+          elements << ControlCommands.get_control_command(value)
+          next
+        end
+
+        elements << Values.parse(value)
       end
     end
 
@@ -40,7 +54,7 @@ module RubyRedInk
 
       final_attribute.each do |key, nested_container|
         next if key == "#n" || key == "#f"
-        nested_containers[key] = self.class.new(nested_container)
+        nested_containers[key] = self.class.new(nested_container, path_string, key)
       end
     end
 
