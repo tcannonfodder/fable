1bedfe497d7daec215cca547c103e7b86ae1515b
Thomas Cannon <tcannon00@gmail.com>
2020-02-09 22:21:20 -0500

    First attempts at building an evaluation stack

--- lib/ruby_red_ink/call_stack.rb
+++ lib/ruby_red_ink/call_stack.rb
@@ -1,10 +1,11 @@
 module RubyRedInk
   class CallStack
-    attr_accessor :current_stack_index, :container_stack
+    attr_accessor :current_stack_index, :container_stack, :evaluation_stacks
 
     def initialize(container_stack)
       @current_stack_index = 0
       @container_stack = container_stack
+      @evaluation_stacks = []
     end
 
     def step
@@ -19,7 +20,7 @@ module RubyRedInk
         }
       end
 
-      if current_stack_element.nil? || current_stack_element == ControlCommands.get_control_command('done')
+      if current_stack_element.nil? || current_stack_element == ControlCommands::COMMANDS[:DONE]
         return {
           action: :pop_stack,
           element: current_stack_element,
@@ -27,11 +28,54 @@ module RubyRedInk
         }
       end
 
+      if ControlCommands::COMMANDS.has_key?(current_stack_element)
+        if current_stack_element == :BEGIN_LOGICAL_EVALUATION_MODE
+          evaluation_stack = EvaluationStack.new
+          self.evaluation_stacks << evaluation_stack
+          self.current_stack_index += 1
+
+          reached_end = false
+          items_in_evaluation_stack = []
+          debugger
+          while !reached_end
+            next_item = container_stack.elements[self.current_stack_index]
+
+            if next_item == :END_LOGICAL_EVALUATION_MODE
+              reached_end = true
+            else
+              items_in_evaluation_stack << next_item
+            end
+            self.current_stack_index += 1
+          end
+
+          debugger
+          items_in_evaluation_stack
+        end
+      end
+
       return {
         action: :output,
         element: current_stack_element,
         path: current_stack_path
       }
+    end
+
+    def process_control_command(current_stack_element)
+
+    end
+  end
+
+  class EvaluationStack
+    include ControlCommands
+
+    attr_accessor :stack
+
+    def intialize
+      stack =[]
+    end
+
+    def add_to_stack(value)
+      stack << value
     end
   end
 end
