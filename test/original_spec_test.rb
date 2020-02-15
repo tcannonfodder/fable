1b97113a088ed9deed79b4d50d6965b42903eb4e
Thomas Cannon <tcannon00@gmail.com>
2020-02-14 20:11:15 -0500

    Start porting tests from canonical ink repo

---
+++ test/original_spec_test.rb
@@ -0,0 +1,34 @@
+require_relative "test_helper"
+
+# tests ported from the main Ink API
+class OriginalSpecTest < Minitest::Test
+  def test_arithmetic
+    json = load_json_export("test/fixtures/arithmetic.ink.json")
+    story = RubyRedInk::Story.new(json)
+
+    assert_nil story.engine.step
+    assert_equal "36", story.engine.step
+    assert_equal "\n", story.engine.step
+    assert_equal "2", story.engine.step
+    assert_equal "\n", story.engine.step
+    assert_equal "3", story.engine.step
+    assert_equal "\n", story.engine.step
+    assert_equal "2", story.engine.step
+    assert_equal "\n", story.engine.step
+    assert_equal "2.333333", story.engine.step
+    assert_equal "\n", story.engine.step
+    assert_equal "8", story.engine.step
+    assert_equal "\n", story.engine.step
+    assert_equal "8", story.engine.step
+    assert_equal "\n", story.engine.step
+    # 36
+    # \n
+    # 2
+    # \n
+    # 3
+    # \n
+    # 2
+    # \n
+    # 2
+  end
+end
