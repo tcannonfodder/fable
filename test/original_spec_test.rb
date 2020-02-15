ee2b2474443168414d6f467988101707a710cdb5
Thomas Cannon <tcannon00@gmail.com>
2020-02-15 10:52:55 -0500

    Rewrite canonical spec tests to use new patterns

--- test/original_spec_test.rb
+++ test/original_spec_test.rb
@@ -6,35 +6,40 @@ class OriginalSpecTest < Minitest::Test
     json = load_json_export("test/fixtures/arithmetic.ink.json")
     story = RubyRedInk::Story.new(json)
 
-    assert_equal "36", story.engine.step
-    assert_equal "\n", story.engine.step
-    assert_equal "2", story.engine.step
-    assert_equal "\n", story.engine.step
-    assert_equal "3", story.engine.step
-    assert_equal "\n", story.engine.step
-    assert_equal "2", story.engine.step
-    assert_equal "\n", story.engine.step
-    assert_equal "2.333333", story.engine.step
-    assert_equal "\n", story.engine.step
-    assert_equal "8", story.engine.step
-    assert_equal "\n", story.engine.step
-    assert_equal "8", story.engine.step
-    assert_equal "\n", story.engine.step
+    assert_nil story.engine.step
+
+    result = <<~STORY
+    36
+    2
+    3
+    2
+    2.333333
+    8
+    8
+    STORY
+
+    assert_equal result, story.engine.current_text
   end
 
   def test_basic_string_literals
     json = load_json_export("test/fixtures/basic-string-literals.ink.json")
     story = RubyRedInk::Story.new(json)
 
-    assert_equal "Hello world 1\n", story.engine.step_until_newline
-    assert_equal "Hello world 2.\n", story.engine.step_until_newline
+    assert_nil story.engine.step
+
+    result = <<~STORY
+    Hello world 1
+    Hello world 2.
+    STORY
+
+    assert_equal result, story.engine.current_text
   end
 
   def test_basic_tunnel
     json = load_json_export("test/fixtures/basic-tunnel.ink.json")
     story = RubyRedInk::Story.new(json)
-    
-    assert_equal "Hello\n", story.engine.step_until_newline
-    debugger
+
+    assert_nil story.engine.step
+    assert_equal "Hello world\n", story.engine.current_text
   end
 end
