406e8cbeff6e918905e4bac6bcbc5b0197069114
Thomas Cannon <tcannon00@gmail.com>
2020-02-15 15:16:43 -0500

    Add test for `blanks-in-inline-sequence`

--- test/original_spec_test.rb
+++ test/original_spec_test.rb
@@ -42,4 +42,33 @@ class OriginalSpecTest < Minitest::Test
     assert_nil story.engine.step
     assert_equal "Hello world\n", story.engine.current_text
   end
+
+  def test_blanks_in_inline_sequences
+    json = load_json_export("test/fixtures/original-specs/blanks-in-inline-sequences.ink.json")
+    story = RubyRedInk::Story.new(json)
+
+    assert_nil story.engine.step
+
+    result = <<~STORY
+    1. a
+    2.
+    3. b
+    4. b
+    ---
+    1.
+    2. a
+    3. a
+    ---
+    1. a
+    2.
+    3.
+    ---
+    1.
+    2.
+    3.
+
+    STORY
+    puts story.engine.current_text
+    assert_equal result, story.engine.current_text
+  end
 end
