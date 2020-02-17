45946fdd8ecedceb6d3f7adadd8c1f5d6391ec1d
Thomas Cannon <tcannon00@gmail.com>
2020-02-16 22:20:45 -0500

    Support `VariableTargetDivert` in Eval mode

--- lib/ruby_red_ink/call_stack.rb
+++ lib/ruby_red_ink/call_stack.rb
@@ -256,6 +256,12 @@ module RubyRedInk
               run_embedded_engine(next_item.target)
             when StandardDivert
               process_standard_divert(next_item)
+              next
+            when VariableTargetDivert
+              puts "#{print_padding}RUNNING EVAL MODE VARIABLE TARGET DIVERT"
+              target_divert = state.get_variable_value(next_item.target)
+              process_standard_divert(target_divert)
+              next
             else
               evaluation_stack.push(next_item)
             end
