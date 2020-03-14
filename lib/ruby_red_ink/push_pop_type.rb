023ab3d8052a81fb2095a96961c75eba69d7ddc8
Thomas Cannon <tcannon00@gmail.com>
2020-03-13 23:27:48 -0400

    First pass at porting CallStack

---
+++ lib/ruby_red_ink/push_pop_type.rb
@@ -0,0 +1,11 @@
+module RubyRedInk
+  module PushPopType
+    TYPES = {
+      tunnel: 0,
+      function: 1,
+      function_evaluation_from_game: 2
+    }
+
+    TYPE_LOOKUP = TYPES.invert
+  end
+end
